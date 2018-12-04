f262498a854d4f7ccc456999eaa3eadc88ef4fa3
Francisco Bosch Ruiz <contacto@franciscobosch.es>
2018-12-04 01:30:46 +0100

    Importación de iconos

---
+++ src/pages/Medicines.vue
@@ -0,0 +1,51 @@
+<template>
+    <f7-page>
+        <f7-navbar title="ALERGIAS" back-link="Volver"></f7-navbar>
+        <f7-block inner>
+            <f7-list>
+                <f7-list-item
+                        v-for="listItem in list"
+                        :key="listItem._id"
+                        :link="/allergies/ + listItem._id"
+                        :header="listItem.type"
+                        :title="listItem.name"
+                        :footer="listItem.degree"></f7-list-item>
+            </f7-list>
+        </f7-block>
+    </f7-page>
+</template>
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: 'Allergies',
+        props: [],
+        data() {
+            return {
+                list: []
+            };
+        },
+        methods: {
+            //
+        },
+        mounted() {
+            axios
+                .get('http://patbookapi.local/api/allergies', {
+                    params: {
+                        device_code: sessionStorage.device_code,
+                        user_id: sessionStorage.user_id
+                        // TODO: encriptar las credenciales?
+                    }
+                })
+                .then(response => (this.list = response.data))
+                .catch(error => (console.log(error)));
+        }
+    }
+    ;
+</script>
+
+<style scoped>
+    .navbar {
+        background-color: #D3698E;
+    }
+</style>
