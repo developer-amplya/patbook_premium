d40f65cde68fc10e050e93e5ffa926cc40905212
Francisco Bosch Ruiz <contacto@franciscobosch.es>
2019-01-03 16:25:46 +0100

    Se completan los formulario de inserción

--- src/pages/MedicalVisitsInsert.vue
+++ src/pages/MedicalVisitsInsert.vue
@@ -1,16 +1,191 @@
 <template>
     <f7-page>
-        <f7-navbar title="VISITA MÉDICA" back-link="Volver"></f7-navbar>
+        <f7-navbar title="CREAR VISITA MÉDICA" back-link="Volver"></f7-navbar>
         <f7-block inner>
             <f7-list>
-                <f7-list-item
-                        v-for="listItem in list"
-                        :key="listItem._id"
-                        :link="/medical-visits/ + listItem._id"
-                        :header="listItem.type"
-                        :title="listItem.name"
-                        :footer="listItem.degree"></f7-list-item>
+
+                <!-- NAME -->
+                <f7-list-item>
+                    <f7-label>Nombre</f7-label>
+                    <f7-input
+                            type="text"
+                            :value="name"
+                            @input="name = $event.target.value"
+                            clear-button></f7-input>
+                </f7-list-item>
+
+                <!-- DESCRIPTION -->
+                <f7-list-item>
+                    <f7-label>Descripción</f7-label>
+                    <f7-input
+                            type="textarea"
+                            :value="description"
+                            @input="description = $event.target.value"
+                    ></f7-input>
+                </f7-list-item>
+
+                <!-- DOCTOR -->
+                <f7-list-item>
+                    <f7-label>Médico</f7-label>
+                    <f7-input
+                            type="text"
+                            :value="doctor"
+                            @input="doctor = $event.target.value"
+                            clear-button></f7-input>
+                </f7-list-item>
+
+                <!-- LOCATION -->
+                <f7-list-item>
+                    <f7-label>Centro</f7-label>
+                    <f7-input
+                            type="text"
+                            :value="location"
+                            @input="location = $event.target.value"
+                            clear-button></f7-input>
+                </f7-list-item>
+
+                <!-- ADDRESS -->
+                <f7-list-item>
+                    <f7-label>Dirección</f7-label>
+                    <f7-input
+                            type="text"
+                            :value="address"
+                            @input="address = $event.target.value"
+                            clear-button></f7-input>
+                </f7-list-item>
+
+                <!-- PHONE -->
+                <f7-list-item>
+                    <f7-label>Teléfono</f7-label>
+                    <f7-input
+                            type="text"
+                            :value="phone"
+                            @input="phone = $event.target.value"
+                            clear-button></f7-input>
+                </f7-list-item>
+
+                <!-- SPECIALITY -->
+                <f7-list-item smart-select title="Especialidad" :smart-select-params="{ closeOnSelect: true }">
+                    <select :name="speciality" v-model="speciality">
+                        <option v-for="(item, index) in specialityList"
+                                :key="index"
+                                :value="item"
+                        >{{ item }}
+                        </option>
+                    </select>
+                </f7-list-item>
+
+                <!-- DATE -->
+
+                <!-- TIME -->
+
+                <!-- CAUSE -->
+                <f7-list-item>
+                    <f7-label>Motivo</f7-label>
+                    <f7-input
+                            type="text"
+                            :value="cause"
+                            @input="cause = $event.target.value"
+                            clear-button></f7-input>
+                </f7-list-item>
+
+                <!-- CONTRIBUTED -->
+                <f7-list-item>
+                    <f7-label>Pruebas e informes aportados</f7-label>
+                    <f7-input
+                            type="textarea"
+                            :value="contributed"
+                            @input="contributed = $event.target.value"
+                    ></f7-input>
+                </f7-list-item>
+
+                <!-- PRESCRIPTION -->
+                <f7-list-item>
+                    <f7-label>Prescripción - Medicación</f7-label>
+                    <f7-input
+                            type="text"
+                            :value="prescription"
+                            @input="prescription = $event.target.value"
+                            clear-button></f7-input>
+                </f7-list-item>
+
+                <!-- TREATMENT -->
+                <f7-list-item>
+                    <f7-label>Prescripción - Tratamiento</f7-label>
+                    <f7-input
+                            type="textarea"
+                            :value="treatment"
+                            @input="treatment = $event.target.value"
+                    ></f7-input>
+                </f7-list-item>
+
+                <!-- DIAGNOSTIC TESTS -->
+                <f7-list-item>
+                    <f7-label>Prescripción - Pruebas diagnósticas</f7-label>
+                    <f7-input
+                            type="text"
+                            :value="diagnostic_tests"
+                            @input="diagnostic_tests = $event.target.value"
+                            clear-button></f7-input>
+                </f7-list-item>
+
+                <!-- RECOMENDATIONS -->
+                <f7-list-item>
+                    <f7-label>Recomendaciones</f7-label>
+                    <f7-input
+                            type="textarea"
+                            :value="recomendations"
+                            @input="recomendations = $event.target.value"
+                    ></f7-input>
+                </f7-list-item>
+
+                <!-- DIAGNOSTIC -->
+                <f7-list-item>
+                    <f7-label>Diagnóstico</f7-label>
+                    <f7-input
+                            type="text"
+                            :value="diagnostic"
+                            @input="diagnostic = $event.target.value"
+                            clear-button></f7-input>
+                </f7-list-item>
+
+                <!-- DISEASE -->
+                <f7-list-item>
+                    <f7-label>Enfermedad</f7-label>
+                    <f7-input
+                            type="text"
+                            :value="disease"
+                            @input="disease = $event.target.value"
+                            clear-button></f7-input>
+                </f7-list-item>
+
+                <!-- COMMENTS -->
+                <f7-list-item>
+                    <f7-label>Comentarios</f7-label>
+                    <f7-input
+                            type="textarea"
+                            :value="comments"
+                            @input="comments = $event.target.value"
+                    ></f7-input>
+                </f7-list-item>
+
+                <!-- NEXT VISIT DATE -->
+
+                <!-- NEXT VISIT TIME -->
+
             </f7-list>
+
+            <f7-button>NUEVO CAMPO PERSONALIZADO</f7-button>
+
+            <br>
+            <br>
+
+            <!-- Cancel & Submit -->
+            <f7-segmented round raised>
+                <f7-button round @click="cancel()">Cancelar</f7-button>
+                <f7-button round @click="insert()">Guardar</f7-button>
+            </f7-segmented>
+
         </f7-block>
     </f7-page>
 </template>
@@ -18,30 +193,78 @@
     import axios from 'axios';
 
     export default {
-        name: 'MedicalVisits',
+        name: 'MedicalVisitsInsert',
         props: [],
         data() {
             return {
-                list: []
+                specialityList: ['Alergología', 'Anestesiología y reanimación', 'Cardiología', 'Endocrinología', 'Gastroenterología', 'Geriatría', 'Ginecología', '...'],
+                name: '',
+                description: '',
+                doctor: '',
+                location: '',
+                address: '',
+                phone: '',
+                speciality: '',
+                date: '',
+                time: '',
+                cause: '',
+                contributed: '',
+                prescription: '',
+                treatment: '',
+                diagnostic_tests: '',
+                recomendations: '',
+                diagnostic: '',
+                disease: '',
+                comments: '',
+                nex_visit_date: '',
+                next_visit_time: '',
+                schema: []
             };
         },
         methods: {
-            //
-        },
-        mounted() {
-            axios
-                .get('http://patbookapi.local/api/medical-visits', {
-                    params: {
+            insert() {
+                axios.post('http://patbookapi.local/api/medical-visits', {
+                    /*params: {
                         device_code: sessionStorage.device_code,
                         user_id: sessionStorage.user_id
                         // TODO: encriptar las credenciales?
-                    }
+                    },*/
+                    user_id: sessionStorage.user_id,
+                    name: this.name,
+                    description: this.name,
+                    doctor: this.name,
+                    location: this.name,
+                    address: this.name,
+                    phone: this.name,
+                    speciality: this.name,
+                    date: this.name,
+                    time: this.name,
+                    cause: this.name,
+                    contributed: this.name,
+                    prescription: this.name,
+                    treatment: this.name,
+                    diagnostic_tests: this.name,
+                    recomendations: this.name,
+                    diagnostic: this.name,
+                    disease: this.name,
+                    comments: this.name,
+                    nex_visit_date: this.name,
+                    next_visit_time: this.name,
+                    schema: this.schema
                 })
-                .then(response => (this.list = response.data))
-                .catch(error => (console.log(error)));
+                    .then((response) => {
+                        console.log(response);
+                        this.$f7router.navigate('/medical-visits');
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            }
+        },
+        mounted() {
+            //
         }
-    }
-    ;
+    };
 </script>
 
 <style scoped>
--- src/pages/MedicinesInsert.vue
+++ src/pages/MedicinesInsert.vue
@@ -1,16 +1,140 @@
 <template>
     <f7-page>
-        <f7-navbar title="MEDICACIONES" back-link="Volver"></f7-navbar>
+        <f7-navbar title="CREAR MEDICACIÓN" back-link="Volver"></f7-navbar>
         <f7-block inner>
             <f7-list>
-                <f7-list-item
-                        v-for="listItem in list"
-                        :key="listItem._id"
-                        :link="/medicines/ + listItem._id"
-                        :header="listItem.type"
-                        :title="listItem.name"
-                        :footer="listItem.degree"></f7-list-item>
+
+                <!-- Name -->
+                <f7-list-item>
+                    <f7-label>Nombre</f7-label>
+                    <f7-input
+                            type="text"
+                            :value="name"
+                            @input="name = $event.target.value"
+                            clear-button></f7-input>
+                </f7-list-item>
+
+                <!-- Image -->
+
+                <!-- Laboratory -->
+                <f7-list-item>
+                    <f7-label>Laboratorio</f7-label>
+                    <f7-input
+                            type="text"
+                            :value="lab"
+                            @input="lab = $event.target.value"
+                            clear-button></f7-input>
+                </f7-list-item>
+
+                <!-- Content -->
+                <f7-list-item>
+                    <f7-label>Miligramos/Mililitros</f7-label>
+                    <f7-input
+                            type="text"
+                            :value="content"
+                            @input="content = $event.target.value"
+                            clear-button></f7-input>
+                </f7-list-item>
+
+                <!-- Dose -->
+                <f7-list-item>
+                    <f7-label>Dosis</f7-label>
+                    <f7-input
+                            type="text"
+                            :value="dose"
+                            @input="dose = $event.target.value"
+                            clear-button></f7-input>
+                </f7-list-item>
+
+                <!-- Frequency -->
+                <f7-list-item smart-select title="Frecuencia" :smart-select-params="{ closeOnSelect: true }">
+                    <select :name="frequency" v-model="frequency">
+                        <option v-for="(item, index) in frequencyList"
+                                :key="index"
+                                :value="item"
+                        >{{ item }}
+                        </option>
+                    </select>
+                </f7-list-item>
+
+                <!-- Time -->
+                <f7-list-item>
+                    <f7-label>Hora/Pauta</f7-label>
+                    <f7-input
+                            type="text"
+                            :value="time"
+                            @input="time = $event.target.value"
+                            clear-button></f7-input>
+                </f7-list-item>
+
+                <!-- Day time -->
+                <f7-list-item smart-select title="Momento del día" :smart-select-params="{ closeOnSelect: true }">
+                    <select :name="day_time" v-model="day_time">
+                        <option v-for="(item, index) in dayTimeList"
+                                :key="index"
+                                :value="item"
+                        >{{ item }}
+                        </option>
+                    </select>
+                </f7-list-item>
+
+                <!-- Start date -->
+
+                <!-- Ending date -->
+
+                <!-- Cause -->
+                <f7-list-item>
+                    <f7-label>Motivo</f7-label>
+                    <f7-input
+                            type="text"
+                            :value="cause"
+                            @input="cause = $event.target.value"
+                            clear-button></f7-input>
+                </f7-list-item>
+
+                <!-- Effects -->
+                <f7-list-item>
+                    <f7-label>Efectos</f7-label>
+                    <f7-input
+                            type="textarea"
+                            :value="effects"
+                            @input="effects = $event.target.value"
+                    ></f7-input>
+                </f7-list-item>
+
+                <!-- Notes -->
+                <f7-list-item>
+                    <f7-label>Notas</f7-label>
+                    <f7-input
+                            type="textarea"
+                            :value="notes"
+                            @input="notes = $event.target.value"
+                    ></f7-input>
+                </f7-list-item>
+
+                <!-- Doctor -->
+                <f7-list-item>
+                    <f7-label>Médico</f7-label>
+                    <f7-input
+                            type="text"
+                            :value="doctor"
+                            @input="doctor = $event.target.value"
+                            clear-button></f7-input>
+                </f7-list-item>
+
             </f7-list>
+
+            <f7-button>NUEVO CAMPO PERSONALIZADO</f7-button>
+
+            <br>
+            <br>
+
+            <!-- Cancel & Submit -->
+            <f7-segmented round raised>
+                <f7-button round @click="cancel()">Cancelar</f7-button>
+                <f7-button round @click="insert()">Guardar</f7-button>
+            </f7-segmented>
+
         </f7-block>
     </f7-page>
 </template>
@@ -18,30 +142,67 @@
     import axios from 'axios';
 
     export default {
-        name: 'Medicines',
+        name: 'MedicinesInsert',
         props: [],
         data() {
             return {
-                list: []
+                frequencyList: ['Diaria', 'Semanal', 'Mensual', 'Bimensual', 'Trimestral', 'Otros'],
+                dayTimeList: ['Mañana', 'Tarde', 'Noche'],
+                name: '',
+                image: '',
+                lab: '',
+                content: '',
+                dose: '',
+                frequency: '',
+                time: '',
+                day_time: '',
+                start: '',
+                end: '',
+                cause: '',
+                effects: '',
+                notes: '',
+                doctor: '',
+                schema: []
             };
         },
         methods: {
-            //
-        },
-        mounted() {
-            axios
-                .get('http://patbookapi.local/api/medicines', {
-                    params: {
+            insert() {
+                axios.post('http://patbookapi.local/api/medicines', {
+                    /*params: {
                         device_code: sessionStorage.device_code,
                         user_id: sessionStorage.user_id
                         // TODO: encriptar las credenciales?
-                    }
+                    },*/
+                    user_id: sessionStorage.user_id,
+                    name: this.name,
+                    image: this.name,
+                    lab: this.name,
+                    content: this.name,
+                    dose: this.name,
+                    frequency: this.name,
+                    time: this.name,
+                    day_time: this.name,
+                    start: this.name,
+                    end: this.name,
+                    cause: this.name,
+                    effects: this.name,
+                    notes: this.name,
+                    doctor: this.name,
+                    schema: this.schema
                 })
-                .then(response => (this.list = response.data))
-                .catch(error => (console.log(error)));
+                    .then((response) => {
+                        console.log(response);
+                        this.$f7router.navigate('/medicines');
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            }
+        },
+        mounted() {
+            //
         }
-    }
-    ;
+    };
 </script>
 
 <style scoped>
