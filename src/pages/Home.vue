499efefd6c3e40829d6a9c6c042d5992082b74e3
Francisco Bosch Ruiz <contacto@franciscobosch.es>
2019-01-25 13:13:29 +0100

    Maquetar la cabecera de la home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,32 @@
 <template>
     <f7-page>
 
-        <f7-navbar sliding></f7-navbar>
+        <div class="header-container">
 
-        <!-- USER PIC -->
-        <div class="user-pic">
-            <img :src="USER_PICS_PATH + getUserPic"/>
+            <!-- EDIT USER -->
+            <div class="col">
+                <f7-link href="#" icon-only class="edit-user">
+                    <f7-icon material="create"></f7-icon>
+                </f7-link>
+            </div>
+
+            <!-- USER PIC -->
+            <div class="col">
+                <div class="user-pic">
+                    <img :src="USER_PICS_PATH + getUserPic"/>
+                </div>
+            </div>
+
+            <!-- MORE FUNCTIONS -->
+            <div class="col">
+                <f7-link href="#" icon-only class="more">
+                    <f7-icon material="more_vert"></f7-icon>
+                </f7-link>
+            </div>
+
         </div>
 
-        <f7-block-title>{{ getUserName }}</f7-block-title>
+        <h3 class="user-name">{{ getUserName }}</h3>
 
         <f7-block inner>
 
@@ -62,14 +80,43 @@
 </script>
 
 <style scoped>
+
+    .header-container {
+        display: flex;
+    }
+
+    .header-container .col {
+        flex: 1;
+    }
+
+    .edit-user, .more {
+        padding: 20px;
+        color: #8E8E93;
+    }
+
+    .edit-user i, .more i {
+        font-size: 35px !important;
+    }
+
+    .col:nth-child(3) {
+        text-align: right;
+    }
+
     .user-pic {
-        width: 150px;
-        height: 150px;
-        margin: auto;
+        width: 100px;
+        height: 100px;
+        margin: 20px auto 0px;
         overflow: hidden;
+        border: 3px solid #1ABAD4;
         border-radius: 100%;
     }
+
     .user-pic img {
         width: 100%;
+    }
+
+    .user-name {
+        text-align: center;
+        color: #1ABAD4;
     }
 </style>
