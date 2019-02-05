bdadac7c62ce6f1d462a19919d2deddc882dd20f
Francisco Bosch Ruiz <contacto@franciscobosch.es>
2019-02-06 00:55:44 +0100

    Implementación del control de tipo calendario

---
+++ src/form_elements/Calendar.vue
@@ -0,0 +1,23 @@
+<template>
+    <f7-input type="text" readonly="readonly" id="calendar-modal"></f7-input>
+</template>
+
+<script>
+    export default {
+        name: 'Calendar',
+        props: [],
+        data() {
+            return {
+                //
+            };
+        },
+        mounted() {
+            self.calendarDefault = self.$f7.calendar.create({
+                inputEl: '#calendar-modal',
+            });
+        },
+        methods: {}
+    };
+</script>
+
+<style></style>
