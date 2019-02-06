84be7e3d26efd28643efbafb108204c67ac557a8
Francisco Bosch Ruiz <contacto@franciscobosch.es>
2019-02-06 01:25:15 +0100

    Cambio a URLs no locales

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 // config.js
-export const API_PATH = 'http://patbookapi.local/api/';
-export const USER_PICS_PATH = 'http://patbookapi.local/storage/user_pics/';
+//export const API_PATH = 'http://patbookapi.local/api/';
+export const API_PATH = 'http://pat-book.es/api/';
+//export const USER_PICS_PATH = 'http://patbookapi.local/storage/user_pics/';
+export const USER_PICS_PATH = 'http://pat-book.es/storage/user_pics/';
