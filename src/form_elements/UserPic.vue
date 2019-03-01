36c1176eb67970262bd16e7a07b323d34c50d925
Francisco Bosch Ruiz <contacto@franciscobosch.es>
2019-03-01 09:00:00 +0100

    Build

--- src/form_elements/UserPic.vue
+++ src/form_elements/UserPic.vue
@@ -64,7 +64,7 @@
             },
             setPicture(imagePath) {
                 var image = this.$refs.userPic;
-                image.src = "data:image/jpeg;base64," + imagePath;
+                image.src = 'imagePath';
             }
         }
     }
