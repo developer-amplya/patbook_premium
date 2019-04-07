dd430c2e66fe04a79ba2754c8d541cba412302df
Francisco Bosch Ruiz <contacto@franciscobosch.es>
2019-04-07 19:57:39 +0200

    Build .98

--- src/pages/access/LogIn.vue
+++ src/pages/access/LogIn.vue
@@ -38,7 +38,7 @@
 
         </f7-block>
 
-        <div style="position: absolute; bottom: 10px; right: 10px"><small>1.0.97</small></div>
+        <div style="position: absolute; bottom: 10px; right: 10px"><small>1.0.98</small></div>
 
     </f7-page>
 </template>
