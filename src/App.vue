aa339c2a0cb753cd489c6e375498d4d3d3ee8c03
Francisco Bosch Ruiz <contacto@franciscobosch.es>
2019-05-19 13:31:36 +0200

    Configuration

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,58 @@
 <template>
-  <!-- App -->
-  <f7-app :params="f7params">
+    <!-- App -->
+    <f7-app :params="f7params">
 
-    <!-- Statusbar -->
-    <f7-statusbar></f7-statusbar>
+        <!-- Statusbar -->
+        <f7-statusbar></f7-statusbar>
 
-    <!-- Right Panel -->
-    <f7-panel right cover theme-dark>
-      <f7-view url="/panel-right/"></f7-view>
-    </f7-panel>
+        <!-- Right Panel -->
+        <f7-panel right cover theme-dark>
+            <f7-view url="/panel-right/"></f7-view>
+        </f7-panel>
 
-    <!-- Main View -->
-    <f7-view id="main-view" url="/" main class="safe-areas"></f7-view>
+        <!-- Main View -->
+        <f7-view id="main-view" url="/" main class="safe-areas"></f7-view>
 
-  </f7-app>
+    </f7-app>
 </template>
 
 <script>
-// Import Routes
-import routes from './routes.js'
-// Import store
-import store from './vuex/store'
+    // Import Routes
+    import routes from './routes.js'
+    // Import store
+    import store from './vuex/store'
 
-export default {
-  store,
-  data() {
-    return {
-      // Framework7 parameters here
-      f7params: {
-        id: 'es.patbook.app.free', // App bundle ID
-        name: 'Patbook', // App name
-        theme: 'auto', // Automatic theme detection
-        // App routes
-        routes: routes,
-      },
+    export default {
+        store,
+        data() {
+            return {
+                // Framework7 parameters here
+                f7params: {
+                    id: 'es.patbook.app.free', // App bundle ID
+                    name: 'Patbook', // App name
+                    theme: 'auto', // Automatic theme detection
+                    // App routes
+                    routes: routes,
+                    touch: {
+                        tapHold: true,
+                        materialRipple: true,
+                        activeState: true,
+                        disableContextMenu: false
+                    },
+                    dialog: {
+                        buttonOk: 'Aceptar',
+                        buttonCancel: 'Cancelar'
+                    },
+                    statusbar: {
+                        enabled: true,
+                        overlay: false,
+                        iosOverlaysWebView: false,
+                        scrollTopOnClick: true,
+                        materialBackgroundColor: "#000000",
+                        iosBackgroundColor: "#000000"
+                    },
+                },
+            }
+        }
     }
-  }
-}
 </script>
