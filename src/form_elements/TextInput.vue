80fabe01276611d3da52c484e6e96842d45ce9ea
Francisco Bosch Ruiz <contacto@franciscobosch.es>
2019-05-23 01:18:21 +0200

    Refactorization

--- src/form_elements/TextInput.vue
+++ src/form_elements/TextInput.vue
@@ -1,12 +1,12 @@
 <template>
     <f7-list>
-        <f7-list-item>
-            <f7-label>{{label}}</f7-label>
-            <f7-input :type="type"
-                      :name="name"
-                      :value="value"
-                      @input="$emit('input',$event.target.value)"></f7-input>
-        </f7-list-item>
+        <f7-list-input
+                :type="type"
+                :label="label"
+                :name="name"
+                :value="value"
+                @input="$emit('input',$event.target.value)">
+        </f7-list-input>
     </f7-list>
 </template>
 
