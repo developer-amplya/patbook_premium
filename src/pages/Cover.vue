b97f0a2a533f9041dbc0d3c46d50ecfa776ef0b5
Francisco Bosch Ruiz <contacto@franciscobosch.es>
2019-10-30 19:41:12 +0100

    Build .131

--- src/pages/Cover.vue
+++ src/pages/Cover.vue
@@ -9,7 +9,7 @@
 
             <!-- Version -->
             <div class="version">
-                <small>v. 1.0.130</small>
+                <small>v. 1.0.131</small>
             </div>
         </f7-toolbar>
     </f7-page>
